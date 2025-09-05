c9ba420fab695791c2ad26caa7926077a4a98d95
Andy Nur <abdullahnh2003@gmail.com>
2025-09-05 20:24:29 +0700

    update readme file

--- Tugas/function/main1.py
+++ Tugas/function/main1.py
@@ -1,13 +1,13 @@
 import hitung_uang
+import rangking
+import emoji_mood
 uang = [1000,2000,3000,4000]
 
 print(hitung_uang.nama(uang))
 
-import rangking
 nilai_list= [75,90,60,88,100,55]
 print(rangking.urutkan_nilai(nilai_list))
 
-import emoji_mood
-mood_map=['senang','biasa','sedih','semangat']
+mood_map=['senang','semangat',"biasa","sedih"]
 print(emoji_mood.convert_mood(mood_map))
 
